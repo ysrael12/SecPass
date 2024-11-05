bceaacfc322c01e01ce8f1236fc08b4748725b7a
ysrael12 <85563735+ysrael12@users.noreply.github.com>
2024-11-05 10:53:27 -0300

    readme atualizado

--- Main.js
+++ Main.js
@@ -2,17 +2,21 @@ import chalk from "chalk";
 import figlet from "figlet";
 import { Auth } from "./Authentication/Auth.js";
 
-
-
+//logo and log function creation
 export const log = console.log;
 const secPassLogo = chalk.bold.greenBright(figlet.textSync("SecPass"));
 
-// Sec pass message 
+// Sec pass HomePage
 export default  function homeView(){
+    // show the logo
     log(secPassLogo);
+
+    // show this message
     let msg = chalk.blueBright("Welcome to SecPass ! please create a password to use your password bank");
     log(msg);
-    Auth()
+    
+    // call the Authentication method
+    Auth();
 }
 homeView();
 
